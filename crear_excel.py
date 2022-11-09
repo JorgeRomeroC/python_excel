923f1e984e6be9b9d5d2920068a4b06f6528ad10
jorgeromeroc <jromerocontreras@icloud.com>
2022-11-08 23:02:17 -0300

    Creacion de archivos excel por terminal con python 3.9

---
+++ crear_excel.py
@@ -0,0 +1,18 @@
+import os.path
+import pandas
+from pandas import ExcelWriter
+
+ruta = os.path.dirname(os.path.abspath(__file__))
+
+datos = pandas.DataFrame({
+    "id": [1, 2, 3, 4, 5, 6],
+    "nombre": ["Juan", "Jorge", "Silvana", "Zoe", "Diego", "Jorgito"],
+    "apellido": ["Romero", "Novoa", "Contreras", "Pérez", "Torres", "Martinez"]
+})
+datos = datos[['id', 'nombre', 'apellido']]
+
+write = ExcelWriter(ruta+"/archivo.xlsx")
+
+datos.to_excel(write, "Hoja 1", index=False)
+
+write.save()
